1ed4fa1669b8eeae1064e588680a3eef86cceacb
tomfanning <ml@tomfanning.eu>
2017-06-26 23:06:57 +0100

    Commit Visual Studio application

---
+++ k6bez-analyser-client/k6bez-analyser/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace k6bez_analyser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.startF = new System.Windows.Forms.TextBox();
+            this.endF = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.continuous = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.bestSwr = new System.Windows.Forms.Label();
+            this.bestSwrFreq = new System.Windows.Forms.Label();
+            this.swrPanel = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.swrPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(590, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Sweep";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(531, 344);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(549, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Start";
+            // 
+            // startF
+            // 
+            this.startF.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.startF.Location = new System.Drawing.Point(590, 12);
+            this.startF.Name = "startF";
+            this.startF.Size = new System.Drawing.Size(100, 20);
+            this.startF.TabIndex = 3;
+            this.startF.Text = "14.000";
+            // 
+            // endF
+            // 
+            this.endF.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.endF.Location = new System.Drawing.Point(590, 38);
+            this.endF.Name = "endF";
+            this.endF.Size = new System.Drawing.Size(100, 20);
+            this.endF.TabIndex = 5;
+            this.endF.Text = "14.350";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(549, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "End";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(690, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "MHz";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(690, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "MHz";
+            // 
+            // continuous
+            // 
+            this.continuous.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.continuous.AutoSize = true;
+            this.continuous.Location = new System.Drawing.Point(592, 93);
+            this.continuous.Name = "continuous";
+            this.continuous.Size = new System.Drawing.Size(78, 17);
+            this.continuous.TabIndex = 8;
+            this.continuous.Text = "continuous";
+            this.continuous.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Lowest SWR:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 60);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(19, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "at:";
+            // 
+            // bestSwr
+            // 
+            this.bestSwr.AutoSize = true;
+            this.bestSwr.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bestSwr.Location = new System.Drawing.Point(3, 23);
+            this.bestSwr.Name = "bestSwr";
+            this.bestSwr.Size = new System.Drawing.Size(38, 24);
+            this.bestSwr.TabIndex = 11;
+            this.bestSwr.Text = "1:1";
+            // 
+            // bestSwrFreq
+            // 
+            this.bestSwrFreq.AutoSize = true;
+            this.bestSwrFreq.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bestSwrFreq.Location = new System.Drawing.Point(3, 84);
+            this.bestSwrFreq.Name = "bestSwrFreq";
+            this.bestSwrFreq.Size = new System.Drawing.Size(46, 24);
+            this.bestSwrFreq.TabIndex = 12;
+            this.bestSwrFreq.Text = "freq";
+            // 
+            // swrPanel
+            // 
+            this.swrPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.swrPanel.Controls.Add(this.label5);
+            this.swrPanel.Controls.Add(this.bestSwrFreq);
+            this.swrPanel.Controls.Add(this.label6);
+            this.swrPanel.Controls.Add(this.bestSwr);
+            this.swrPanel.Location = new System.Drawing.Point(552, 140);
+            this.swrPanel.Name = "swrPanel";
+            this.swrPanel.Size = new System.Drawing.Size(167, 117);
+            this.swrPanel.TabIndex = 13;
+            this.swrPanel.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(728, 368);
+            this.Controls.Add(this.swrPanel);
+            this.Controls.Add(this.continuous);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.endF);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.startF);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.swrPanel.ResumeLayout(false);
+            this.swrPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox startF;
+        private System.Windows.Forms.TextBox endF;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox continuous;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label bestSwr;
+        private System.Windows.Forms.Label bestSwrFreq;
+        private System.Windows.Forms.Panel swrPanel;
+    }
+}
+
---
+++ k6bez-analyser-client/k6bez-analyser/Form1.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace k6bez_analyser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool stopping;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Stop")
+            {
+                stopping = true;
+                return;
+            }
+
+            double start, end;
+
+            if (!double.TryParse(startF.Text, out start))
+            {
+                MessageBox.Show("Invalid start frequency");
+                return;
+            }
+
+            if (!double.TryParse(endF.Text, out end))
+            {
+                MessageBox.Show("Invalid end frequency");
+                return;
+            }
+
+            if (start <= 0 || start > 30)
+            {
+                MessageBox.Show("Start frequency out of range");
+                return;
+            }
+
+            if (end <= 0 || end > 30)
+            {
+                MessageBox.Show("End frequency out of range");
+                return;
+            }
+
+            if (end < start)
+            {
+                MessageBox.Show("End frequency must be higher than start");
+                return;
+            }
+
+            DisableControls();
+
+            if (continuous.Checked)
+            {
+                button1.Text = "Stop";
+            }
+            else
+            {
+                button1.Enabled = false;
+            }
+
+            var bw = new BackgroundWorker();
+            bw.DoWork += Bw_DoWork;
+            bw.RunWorkerCompleted += Bw_RunWorkerCompleted;
+            bw.WorkerReportsProgress = true;
+            bw.ProgressChanged += Bw_ProgressChanged;
+            bw.RunWorkerAsync(new WorkArg { Start = start, End = end, Continuous = continuous.Checked, Steps = 50 });
+        }
+
+        private void Bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            var results = (ScanResult[])e.UserState;
+
+            chart1.Series[0].Points.Clear();
+            chart1.ChartAreas[0].AxisX.Minimum = results.Min(r => r.FreqHz) / 1000000.0;
+            chart1.ChartAreas[0].AxisX.Maximum = results.Max(r => r.FreqHz) / 1000000.0;
+            chart1.ChartAreas[0].AxisX.Title = "F (MHz)";
+
+            double rangeMHz = (results.Last().FreqHz - results[0].FreqHz) / 1000000.0;
+
+            if (rangeMHz < 0.1)
+            {
+                chart1.ChartAreas[0].AxisX.Interval = 0.01;
+            }
+            else if (rangeMHz < 3)
+            {
+                chart1.ChartAreas[0].AxisX.Interval = 0.1;
+            }
+            else
+            {
+                chart1.ChartAreas[0].AxisX.Interval = 1;
+            }
+
+            chart1.ChartAreas[0].AxisX.IntervalAutoMode = IntervalAutoMode.FixedCount;
+            chart1.ChartAreas[0].AxisY.Minimum = 0;
+            chart1.ChartAreas[0].AxisY.Maximum = ((int)results.Max(r => r.Swr) * 1.2) + 1;
+            chart1.ChartAreas[0].AxisY.Interval = 1;
+            chart1.ChartAreas[0].AxisY.Title = "SWR";
+            foreach (var item in results)
+            {
+                chart1.Series[0].Points.Add(new DataPoint(item.FreqHz / 1000000.0, item.Swr));
+            }
+
+            double lowestSwr = results.Min(r => r.Swr);
+            var lowestSwrFreqs = new List<int>();
+            for (int i = 0; i < results.Length; i++)
+            {
+                if (results[i].Swr > lowestSwr)
+                {
+                    if (lowestSwrFreqs.Any())
+                        break;
+                    else
+                        continue;
+                }
+
+                lowestSwrFreqs.Add(results[i].FreqHz);
+            }
+
+            bestSwr.Text = string.Format("{0:0.00}:1", Math.Round(lowestSwr, 2));
+            bestSwrFreq.Text = string.Format("{0:0.000}MHz", lowestSwrFreqs.Average()/1000000);
+            swrPanel.Visible = true;
+        }
+
+        private void Bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            button1.Text = "Sweep";
+            button1.Enabled = true;
+            stopping = false;
+            EnableControls();
+        }
+
+        class WorkArg
+        {
+            public double Start { get; set; }
+            public double End { get; set; }
+            public int Steps { get; set; }
+            public bool Continuous { get; set; }
+        }
+
+        private void Bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            WorkArg arg = (WorkArg)e.Argument;
+
+            ScanResult[] results;
+            using (var analyser = new Analyser(SerialPort.GetPortNames().First()))
+            {
+                do
+                {
+                    analyser.StartMhz = arg.Start;
+                    analyser.EndMhz = arg.End;
+                    analyser.Steps = arg.Steps;
+                    results = analyser.Scan();
+
+                    ((BackgroundWorker)sender).ReportProgress(0, results);
+
+                } while (arg.Continuous && !stopping);
+            }
+        }
+
+        private void EnableControls()
+        {
+            startF.Enabled = endF.Enabled = continuous.Enabled = true;
+        }
+
+        private void DisableControls()
+        {
+            startF.Enabled = endF.Enabled = continuous.Enabled = false;
+        }
+    }
+
+    class ScanResult
+    {
+        public int FreqHz { get; set; }
+        public double Swr { get; set; }
+        public int Fwd { get; set; }
+        public int Rev { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0:0.000} MHz: {1:0.00}:1", FreqHz / 1000000.0, Swr);
+        }
+    }
+
+    class Analyser : IDisposable
+    {
+        SerialPort sp;
+
+        public Analyser(string portName)
+        {
+            sp = new SerialPort(portName);
+            sp.BaudRate = 57600;
+            sp.Open();
+            UpdateStatus();
+            Idle = true;
+        }
+
+        public ScanResult[] Scan()
+        {
+            sp.Write(string.Format("{0:0}", StartMhz * 1000000));
+            sp.Write("\rA\r");
+
+            sp.Write(string.Format("{0:0}", EndMhz * 1000000));
+            sp.Write("\rB\r");
+
+            sp.Write(string.Format("{0:0}", Steps));
+            sp.Write("\rN\r");
+
+            sp.Write("\rS\r");
+
+            var results = new List<ScanResult>();
+            var sb = new StringBuilder();
+            string resultLine;
+            while (true)
+            {
+                resultLine = sp.ReadLine();
+
+                if (resultLine.Trim() == "End")
+                {
+                    break;
+                }
+
+                sb.AppendLine(resultLine);
+                string interim = sb.ToString();
+
+                results.Add(new ScanResult
+                {
+                    FreqHz = (int)double.Parse(resultLine.SplitElement(",", 0)),
+                    Swr = double.Parse(resultLine.SplitElement(",", 1)) / 1000.0,
+                    Fwd = (int)double.Parse(resultLine.SplitElement(",", 2)),
+                    Rev = (int)double.Parse(resultLine.SplitElement(",", 3)),
+                });
+            }
+
+            return results.ToArray();
+        }
+
+        public bool Idle { get; set; }
+
+        public double StartMhz { get; set; }
+        public double EndMhz { get; set; }
+        public int Steps { get; set; }
+
+        void UpdateStatus()
+        {
+            sp.ReadTimeout = 1000;
+            while (sp.BytesToRead == 0)
+            {
+                sp.Write("?");
+                Thread.Sleep(500);
+            }
+
+            sp.ReadTimeout = -1;
+
+            /*
+             * Start Freq:1.00
+               Stop Freq:22.00
+               Num Steps:500
+             */
+
+            string line1 = sp.ReadLine();
+            string line2 = sp.ReadLine();
+            string line3 = sp.ReadLine();
+
+            StartMhz = double.Parse(line1.SplitElement(":", 1));
+            EndMhz = double.Parse(line2.SplitElement(":", 1));
+            Steps = int.Parse(line3.SplitElement(":", 1));
+        }
+
+        public void Dispose()
+        {
+            sp.Close();
+            sp.Dispose();
+        }
+    }
+
+    static class Extensions
+    {
+        public static string SplitElement(this string input, string splitOn, int elementIndex)
+        {
+            return input.Split(new[] { splitOn }, StringSplitOptions.RemoveEmptyEntries)[elementIndex];
+        }
+    }
+}
